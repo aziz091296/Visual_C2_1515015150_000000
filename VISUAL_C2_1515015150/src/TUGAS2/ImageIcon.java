ff9cb82a719773b4f8a684024d50085fbf892287
aziz091296 <azissingo.aa@gmail.com>
2017-03-08 20:22:20 +0800

    POSTTEST2

---
+++ VISUAL_C2_1515015150/src/TUGAS2/ImageIcon.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TUGAS2;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author aziz
+ */
+class ImageIcon {
+
+    ImageIcon(Image scaledInstance) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
